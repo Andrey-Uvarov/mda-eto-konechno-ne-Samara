49872f746a913d9ded02b29bdc3dc147bd0e8a89
Dictator <aouvarov8@gmail.com>
2020-11-25 17:22:15 +0200

    Kogda Obeme

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double a, b, c, d, n, x, y, z, res;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cin >> d;
+	cin >> n;
+	double tmp; //temporary
+
+	//tmp = abs(b) / (pow(10, n + 3) + log10(b) / (c - d));
+	//res = 0.25 * (a - b) / (1.0 / 8.0 - tmp);
+	cout << "Result = " << res << endl;
+
+	res = (3 + (pow(2.78, y - 1)) / (abs(y - tan(z)) * x * x + 1); 
+	
+
+	system("pause");
+	return 0;
+}
